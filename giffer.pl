c6ce33ecbe07008c996a740e0f248dd3fb213429
Andrea Montagna <fullmontis@gmail.com>
2014-03-31 22:47:33 +0200

    changed options to reduce file size

--- giffer.pl
+++ giffer.pl
@@ -13,14 +13,15 @@ my $t2 = $ARGV[3] or usage();
 # my $gif = $video;
 # $gif =~ s/\.[0-9a-zA-Z]+$/.gif/;
 
-system("ffmpeg -y -i \"$video\" -ss $t1 -t $t2 -an 123temp%03d.jpg"); # 25 fps
+# quality defaults to 320x240, 15 fps
+system("ffmpeg -y -i \"$video\" -ss $t1 -t $t2 -an -r 15 -s 320x240 123temp%03d.jpg");
 
 my @imagelist = glob "123temp*.jpg";
 my $images = "@imagelist";
 print "Creating animated GIF.......";
-system("convert -delay 1x25 $images -coalesce -layers OptimizeTransparency \"$out\"");
+system("convert -delay 1x15 $images -deconstruct -coalesce -layers optimize -matte \"$out\"");
 
-print "Removing temp files..."
+print "Removing temp files...";
 unlink glob "123temp*.jpg";
 
 print "Done!";
